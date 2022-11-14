ac839fb08372b9b074ca1c536e222034c7e4e162
angikama <angela.akamara@gmail.com>
2022-11-14 20:57:47 +0000

    Saves data into JSON files

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ I have selected the TV show section of the Rotten Tomatoes website because I enj
 The scraper selects 10 TV shows that have been filtered by high Tomatometer score, and availiability on 3 streaming sites: Netflix, Disney + and Amazon Prime.
 
 The Selenium webdriver allows automation of the Google Chrome browser, the methods within the class open up the browser window and naviagte to the website. They also allow the desired filters to be selected to return a list of 10 TV show links.
+
+Data is scraped from each link in the list and recorded in a dictionary. Information on each TV show is included, for example, the title, synopsis and genre. The dictionaries holding the data are saved as JSON files in individual folders for each tv show. 
+
+Using requests and shutil modules, the TV show poster image for each show is downloaded using the url scraped from the individual links.
--- Rotten_Tomatoes_Scraper.py
+++ Rotten_Tomatoes_Scraper.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import datetime
-dt = datetime.now()
 
+from datetime import datetime
+now = datetime.now()
+
+import json
+import os
+
+import requests
+import shutil
 
 ## Scraper Class that will contain all the methods
 class Scraper():
@@ -15,6 +21,7 @@ class Scraper():
         # options.headless = True        Add in when code is complete 
         self.driver = webdriver.Chrome(options = options)
         self.tv_show_links = []
+        self.tv_show_info = {}
     
     def search(self):
         URL = "https://www.rottentomatoes.com/browse/tv_series_browse/sort:popular?page=1"
@@ -30,26 +37,25 @@ class Scraper():
         time.sleep(5)
 
 ## Finds netflix, prime and disney buttons and clicks them (3 most popular/fav streaming)
-    def select_streaming_sites(self):
-        netflix_button = self.driver.find_element(By.XPATH, '//where-to-watch-bubble[@value="netflix"]')
-        netflix_button.click()
-        prime_button = self.driver.find_element(By.XPATH, '//where-to-watch-bubble[@value="amazon_prime"]')
-        prime_button.click()
-        disney_button = self.driver.find_element(By.XPATH, '//where-to-watch-bubble[@value="disney_plus"]')
-        disney_button.click()
-        time.sleep(10)
+#    def select_streaming_sites(self):
+ #       netflix_button = self.driver.find_element(By.XPATH, '//where-to-watch-bubble[@value="netflix"]')
+  #      netflix_button.click()
+  #      prime_button = self.driver.find_element(By.XPATH, '//where-to-watch-bubble[@value="amazon_prime"]')
+  #      prime_button.click()
+  #      disney_button = self.driver.find_element(By.XPATH, '//where-to-watch-bubble[@value="disney_plus"]')
+  #      disney_button.click()
+  #      time.sleep(10)
 
 ## Selects top 10 shows and collects their links in a list
     def top_tv_links(self):
-        tv_shows = self.driver.find_elements(By.XPATH, '//a[@class="js-tile-link"]') ## Selects the movie from the list
-        for i in range(len(tv_shows)):
-            self.tv_show_links.append(tv_shows[i].get_attribute('href'))
-            self.tv_show_links[:10]
+        for i in range(10):
+            tv_shows = self.driver.find_elements(By.XPATH, '//a[@class="js-tile-link"]')
+            url = tv_shows[i].get_attribute('href')
+            self.tv_show_links.append(url)
         time.sleep(5)   
 
 ## Retriving text and image data from the individual links and adding to dictionary
     def tv_show_data(self):
-        tv_show_info = {}
         for i in range(len(self.tv_show_links)):
             show = self.tv_show_links[i]
             self.driver.get(show)
@@ -59,21 +65,43 @@ class Scraper():
             self.tv_network = self.driver.find_element(By.XPATH, '//*[@id="detail_panel"]/div/table/tbody/tr[1]/td[2]')
             self.genre = self.driver.find_element(By.XPATH, '//*[@id="detail_panel"]/div/table/tbody/tr[3]/td[2]')
             self.avg_tomatometer_score = self.driver.find_element(By.XPATH, '//*[@id="tomato_meter_link"]/span/span[2]')
-            self.timestamp = dt
             
-            tv_show_info['show_name'] = self.show_name.text
-            tv_show_info['tv_poster_img'] = self.tv_poster_img.get_attribute('src')
-            tv_show_info['synopsis'] = self.synopsis.text
-            tv_show_info['tv_network'] = self.tv_network.text
-            tv_show_info['genre'] = self.genre.text,
-            tv_show_info['avg_tomatometer_score'] = self.avg_tomatometer_score.text
-            tv_show_info['timestamp'] = self.timestamp
-            
+            self.tv_show_info["show_name"] = self.show_name.text
+            self.tv_show_info["tv_poster_img"] = self.tv_poster_img.get_attribute("src")
+            self.tv_show_info["synopsis"] = self.synopsis.text
+            self.tv_show_info["tv_network"] = self.tv_network.text
+            self.tv_show_info["genre"] = self.genre.text
+            self.tv_show_info["avg_tomatometer_score"] = self.avg_tomatometer_score.text
+            self.tv_show_info["timestamp"] = now.strftime("%d/%m/%Y, %H:%M:%S")
+
+        # Saving as json file in a new folder within raw_data
+            filename = self.tv_show_info["show_name"]
+            if not os.path.exists('raw_data'):
+                os.makedirs('raw_data')
+            if not os.path.exists(f'raw_data/{filename}'):
+                os.makedirs(f'raw_data/{filename}')       
+            with open(f'raw_data/{filename}/data.json', 'w') as output:
+                json.dump(self.tv_show_info, output)
+        
+        # retrieves images and downloads them to the folder
+            image_url = self.tv_show_info["tv_poster_img"]
+            timestamp_now = str(now)
+            image_file_name = timestamp_now+".jpg"
+
+            res = requests.get(image_url, stream = True)
+            if not os.path.exists('raw_data'):
+                os.makedirs('raw_data')
+            if not os.path.exists(f'raw_data/{filename}/images'):
+                os.makedirs(f'raw_data/{filename}/images')   
+            with open(f'raw_data/{filename}/images/{image_file_name}', 'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+    
 
 if __name__ == "__main__":
     webscraper = Scraper()
     webscraper.search()
     webscraper.high_tomatometer()
-    webscraper.select_streaming_sites()
+    #webscraper.select_streaming_sites()
     webscraper.top_tv_links()
     webscraper.tv_show_data()
+  
